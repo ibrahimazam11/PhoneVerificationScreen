1f1373477ee2a2a185745a18dde174402e18dfba
Ibrahim Azam <Ibrahim Azam>
2018-05-08 10:36:01 +0500

    commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, ImageBackground, Image, ScrollView } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Body, Text, Input } from 'native-base';
+import { StyleSheet, View, ImageBackground, Image, ScrollView, TextInput } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Body, Text, Button, Icon } from 'native-base';
 
 export default class App extends React.Component {
   render() {
@@ -11,14 +11,19 @@ export default class App extends React.Component {
           source={require('./img/login_bg.png')}
           style={{ width: '100%', height: '100%' }}
         >
-          <Container style={styles.mainCard}>
+          <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
             <View style={styles.header} >
               <Image
                 source={require('./img/LOGO.png')}
                 style={{ width: '85%', height: '100%', margin: 5 }}
               />
             </View>
-            <Content>
+            <View style={{ width: '80%', height: 293, top: '-8%', zIndex: 0 }}>
               <Card style={{ borderRadius: 10 }}>
                 <CardItem style={{ marginTop: 45 }}>
                   <Body>
@@ -27,18 +32,19 @@ export default class App extends React.Component {
                   </Body>
                 </CardItem>
                 <CardItem style={{ marginTop: -15 }}>
-                  <Input placeholder="Mobile Number" underlineColorAndroid="#E53935">+92</Input>
+                  <TextInput placeholder="Mobile Number" underlineColorAndroid="#E53935" style={{width: '80%'}}>+92</TextInput>
+                  <Button rounded style={{ backgroundColor: "#E53935" }}><Icon name='md-arrow-forward' /></Button>
                 </CardItem>
                 <CardItem style={{ backgroundColor: "#E53935", margin: 5, borderRadius: 8 }}>
                   <Body>
                     <Text style={{ alignSelf: 'center', color: 'white', fontSize: 16 }}>Enter your 6-Digit Code here</Text>
                     <Text style={{ alignSelf: 'center', color: '#fff', fontSize: 11, fontStyle: 'italic' }}>as soon as you recieve</Text>
-                    <Input style={{ backgroundColor: "#fff", width: '60%', height: 38, borderRadius: 10, alignSelf: 'center', fontSize: 14, marginTop: 5, textAlign: 'center' }} placeholder="-  -  -  -  -  -" />
+                    <TextInput underlineColorAndroid="transparent" style={{ backgroundColor: "#fff", width: '60%', height: 36, borderRadius: 10, alignSelf: 'center', fontSize: 14, marginTop: 7, textAlign: 'center', color:"black" }} placeholder="-  -  -  -  -  -" />
                   </Body>
                 </CardItem>
               </Card>
-            </Content>
-          </Container>
+            </View>
+          </View>
         </ImageBackground>
       </View>
     );
@@ -49,28 +55,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
 
   },
   mainCard: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    width: '80%',
-    position: 'absolute'
+
   },
   header: {
     flexDirection: 'row',
+    alignItems: 'center',
     alignSelf: 'center',
     justifyContent: 'center',
     backgroundColor: '#E53935',
     height: 90,
-    width: '80%',
+    width: '65%',
     borderRadius: 8,
-    position: 'absolute',
-    top: 100,
     zIndex: 1
   }
 });
